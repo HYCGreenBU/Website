13c6e642bc55a936c489fb18dda39359699490f4
Jonathan <jonathan.joe2745@gmail.com>
2024-09-06 15:37:39 +0800

    Website under developemnt message

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,26 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
-import { getSortedPostsData } from '../lib/posts';
+// import { getSortedPostsData } from '../lib/posts';
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData();
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
 
 
 
 export default function Home({ allPostsData }) {
   return (
-    <Layout home>
-      {/* Keep the existing code here */}
 
-      {/* Add this <section> tag below the existing <section> tag */}
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h1> Website under devlopment, Please come back later </h1>
+
+
+      /* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
@@ -34,7 +34,7 @@ export default function Home({ allPostsData }) {
             </li>
           ))}
         </ul>
-      </section>
-    </Layout>
+      </section>  */
+
   );
 }
