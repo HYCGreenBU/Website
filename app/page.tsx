79fb059fc5083136fec228c0ef37305e0f835ee4
Jonathan <jonathan.joe2745@gmail.com>
2024-09-13 14:00:01 +0800

    Added Barebones Content

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,92 @@
+import Image from "next/image";
+import Link from "next/link";
+import Navbar from "./components/navigation/navbar";
+
+import placeholderimage from "../public/media/Images/placeholderimage.jpg";
 
 export default function Home() {
   return (
     <>
-      <div className="container mx-auto px-4">
-        <h1>Hello word</h1>
-        <p>This is a content to make our page longer</p>
-        <div className="w-full h-screen bg-green-300"></div>
-        <p>
-          Lorem Ipsum is simply dummy text ...
-        </p>
+      {<Navbar/>}
+      <div className="container mx-auto px-4 py-8">
+        <section>
+          <p className="text-5xl text-center"> What We Do </p>
+          <p className="text-3xl text-center"> We are dedicated to delivering tailor-made technology solutions that precisely align with your unique business needs. Our commitment begins with a thorough consultation and needs assessment to understand your challenges, objectives, and operational requirements.</p>
+        </section>
+
+        <section>
+          <p className="text-5xl text-center"> Our Products </p>\
+          <Image 
+          src={placeholderimage} // Replace with your image path
+          alt="Descriptive Alt Text"
+          className="w-auto h-auto object-cover"
+          />
+          <p className="text-3xl text-center"> Theros </p>
+          <p className="text-2xl text-center"> Scalable IR oven that is customizable to 3,4 or 5 tiers </p>
+          <div className="flex justify-center">
+            <Link href="/pages/Products/Theros" passHref>
+              <button className="px-4 py-2 bg-black text-white rounded align-center">
+                View More
+              </button>
+            </Link>
+          </div>
+        </section>
+
+        <section>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div>
+              <Image 
+                src={placeholderimage} // Replace with your image path
+                alt="Image 1"
+                width={600}
+                height={400}
+                className="w-full h-auto object-cover"
+              />
+              <p className="text-lg">Cooling</p>
+              <div className="flex justify-center">
+                <Link href="/pages/Products/Cooling" passHref>
+                  <button className="px-4 py-2 bg-black text-white rounded align-center">
+                    View More
+                  </button>
+                </Link>
+              </div>
+            </div>
+            <div>
+              <Image 
+                src={placeholderimage} // Replace with your image path
+                alt="Image 2"
+                width={600}
+                height={400}
+                className="w-full h-auto object-cover"
+              />
+              <p className="text-lg">Vision</p>
+              <div className="flex justify-center">
+                <Link href="/pages/Products/Vision" passHref>
+                  <button className="px-4 py-2 bg-black text-white rounded align-center">
+                    View More
+                  </button>
+                </Link>
+              </div>
+            </div>
+            <div>
+              <Image 
+                src={placeholderimage} // Replace with your image path
+                alt="Image 3"
+                width={600}
+                height={400}
+                className="w-full h-auto object-cover"
+              />
+              <p className="text-lg">Stacking</p>
+              <div className="flex justify-center">
+                  <Link href="/pages/Products/Theros" passHref>
+                    <button className="px-4 py-2 bg-black text-white rounded align-center">
+                      View More
+                    </button>
+                  </Link>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
--- app/pages/Products/Theros/page.tsx
+++ app/pages/Products/Theros/page.tsx
@@ -18,8 +18,8 @@ const Theros = () => {
       </section>
       
       {/* Video Section */}
-      <section className="mb-8">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/u31qwQUeGuM?si=0s0X583WBdYv47D1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <section className="mb-8" >
+        <iframe className="w-full aspect-[16/9]" src="https://www.youtube.com/embed/u31qwQUeGuM?si=0s0X583WBdYv47D1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </section>
       
       {/* Text Section */}
@@ -30,38 +30,77 @@ const Theros = () => {
         </p>
       </section>
       
-      {/* Additional Images Section */}
+      {/* Product array Section */}
       <section>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           <div>
             <Image 
-              src="/path/to/your/image1.jpg" // Replace with your image path
+              src={placeholderimage} // Replace with your image path
               alt="Image 1"
               width={600}
               height={400}
               className="w-full h-auto object-cover"
             />
+            <p className="text-lg">Our system features modular heater boxes for easy replacements, efficient IR heating, precise temperature control, and the capability to reach higher temperatures than conventional ovens, all while remaining energy efficient.</p>
           </div>
           <div>
             <Image 
-              src="/path/to/your/image2.jpg" // Replace with your image path
+              src={placeholderimage} // Replace with your image path
               alt="Image 2"
               width={600}
               height={400}
               className="w-full h-auto object-cover"
             />
+            <p className="text-lg">Designed with scalability in mind, it allows varying numbers of zones (7, 9, 11). This flexibility more configuration of curing times to suit diverse curing time requirements, providing a highly adaptable solution for your specific needs.</p>
           </div>
           <div>
             <Image 
-              src="/path/to/your/image3.jpg" // Replace with your image path
+              src={placeholderimage} // Replace with your image path
               alt="Image 3"
               width={600}
               height={400}
               className="w-full h-auto object-cover"
             />
+            <p className="text-lg">Employs a single belt design to eliminate product transfer issues, reducing complications. It also requires fewer motors compared to its predecessor, resulting in enhanced energy efficiency. Additionally, we've enhanced the take-up design to decrease the frequency of belt cutting, reducing maintenance work.</p>
           </div>
         </div>
       </section>
+
+
+      <section>
+        <Image           
+              src={placeholderimage} // Replace with your image path
+              alt="Image 4"
+              className="w-full h-auto object-cover"
+            />
+      </section>
+      
+      <section>
+        <Image           
+              src={placeholderimage} // Replace with your image path
+              alt="Image 4"
+              className="w-full h-auto object-cover"
+            />
+      </section>
+
+      <section>
+        <p className="text-7xl text-center"> Features </p>
+      </section>
+
+      <section className="mb-8">
+        <Image 
+          src={placeholderimage} // Replace with your image path
+          alt="Descriptive Alt Text"
+          width={1200} // Adjust width as needed
+          height={600} // Adjust height as needed
+          className="w-full h-auto object-cover"
+        />
+      </section>
+
+      <section>
+        <p className="text-5xl text-center"> We also offer after sales service for peace of mind </p>
+      </section>
+      
     </div>
   );
 };
